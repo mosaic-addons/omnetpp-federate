aeb21259e3c80fd1a5c7f778f03996041f9ebfd2
Felix Hilgerdenaar <felix.hilgerdenaar@fokus.fraunhofer.de>
2021-08-06 15:52:43 +0200

    class ClientServerChannel copied from ns-3 federate
    simple implementation of basic ns-3 logging

---
+++ src/util/Ns3Log.cc
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2021 Fraunhofer FOKUS and others. All rights reserved.
+ *
+ * Contact: mosaic@fokus.fraunhofer.de
+ *
+ * This class is developed for the MOSAIC-NS-3 coupling.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+ 
+ #include "Ns3Log.h"
+
+bool is_ns_log_enabled(int level) {
+  if (g_log_level || level)
+    return true;
+  else
+    return false;
+}
+
+
+namespace ns3 {
+
+// copied from ns-3.34, file: core/log.cc
+ParameterLogger::ParameterLogger (std::ostream &os)
+  : m_first (true),
+    m_os (os)
+{}
+
+// copied from ns-3.34, file: core/log.cc
+template<>
+ParameterLogger &
+ParameterLogger::operator<< <std::string> (const std::string param)
+{
+  if (m_first)
+    {
+      m_os << "\"" << param << "\"";
+      m_first = false;
+    }
+  else
+    {
+      m_os << ", \"" << param << "\"";
+    }
+  return *this;
+}
+
+// copied from ns-3.34, file: core/log.cc
+template<>
+ParameterLogger &
+ParameterLogger::operator<< <const char *> (const char * param)
+{
+  (*this) << std::string (param);
+  return *this;
+}
+
+
+}  // namespace ns3
